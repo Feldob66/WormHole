8bc91b17eb8f4aff5e06730f1e4f6958ed86f250
Sin <sinthesinner@proton.me>
2025-03-31 15:17:55 -0400

    added modsdk and main.ts

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import bcModSDK from 'bondage-club-mod-sdk';
+
+const VERSION = "0.0.0.1";
+const NAME = "wormhole";
+const NICKNAME = "Wormhole';
+
+const WORMHOLE = bcModSDK.registerMod({
+    name: NICKNAME,
+    fullname: NAME,
+    version: VERSION,
+    repostitory: "",
+});
+
